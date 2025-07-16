7658a08912174fd14d965b2a4f171846a4c3984e
samcursive <saamerarshad@gmail.com>
2025-07-16 08:23:01 -0500

    Created a loop to get input from terminal and see if 'shell.c' can flag multi-input passing.
    Added an exit statement where a user may enter "exit" and the shell itself will terminate.

--- shell.c
+++ shell.c
@@ -1,3 +1,36 @@
 //
 // Created by sam on 7/15/25.
 //
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+main()
+{
+	char *path, *argv[20], buf[80], n, *p;
+	int m, status, inword, continu;
+
+	while (1)
+	{
+
+	inword=m=continu=0;
+	p=buf;
+//	m=0;
+//	continu = 0;
+
+	printf("\nshhh>");
+
+	while( (n = getchar()) != '\n' || continu)
+	 {
+
+		continue;
+
+	 }
+
+	if (strcmp(argv[0], "exit") == 0) {exit = 0}
+
+
+    }
+}
